c9f4c00aa89e1ced9784c397e1b3a5e1d543fccf
Brian <brian.salkas@gmail.com>
2022-12-25 13:57:33 -0600

    add binomial test flag and display pvalue for binomial test

--- sql_or_sequal.py
+++ sql_or_sequal.py
@@ -10,6 +10,7 @@ parser.add_argument('--limit','-l', type=int,default=100,metavar='N', help='limi
 parser.add_argument('--chart', action='store_true', help='create a barchar of the results')
 parser.add_argument('--verbose','-v',action='store_true', help='log username and subreddit of each match to stderr')
 parser.add_argument('--chisquare',type=float, default=None, const=1, nargs='?', help='perform chisquared goodness of fit test.  optional argument is ratio of SQL to Sequal, for example if you expect that the ratio of SQL to Sequal is 2:1, you would give --chisquare 2, if you expect 1 SQL for every 3 Sequals, --chisqare .3333333333')
+parser.add_argument('--binomial', type=float, default=None, const=.5, nargs='?', help='perform binomail test.  optional argument is probability of user pronouncing it as SQL, if you expect 1 SQL for every 3 Sequals, --binomial .333333')
 args = parser.parse_args()
 if args.limit <= 0:
     print(f'argument to limit must be a non-negative integer',file=sys.stderr)
@@ -36,4 +37,10 @@ if args.chisquare is not None:
     res = stats.chisquare(o,e)
     print(f'chi-square GOF pvalue: {res.pvalue}') 
     print(f'chi-square GOF test statistic: {res.statistic}')
+if args.binomial is not None:
+    successes = sql_dict['SQL']
+    n = sql_dict['SQL'] + sql_dict['Sequal']
+    predicted_sucess_prob = args.binomial
+    res = stats.binomtest(successes,n,predicted_sucess_prob)
+    print(f'binomial test pvalue: {res.pvalue}')
 
