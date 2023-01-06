b0db328f87058aa9c2e7269a3cda8fea15a9eb41
Brian <brian.salkas@gmail.com>
2023-01-06 00:45:10 -0600

    add title and labels to pyplot lib

--- pyplot_lib.py
+++ pyplot_lib.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
 def create_sql_vs_sequal_chart(sql_dict):
     plt.bar(sql_dict.keys(), sql_dict.values())
+    plt.title('SQL vs. Sequal')
     plt.show()
+    plt.label(['SQL','Sequal'])
